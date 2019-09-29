b5816746e121d9bef66bf5547b61ff8a7ecc8905
markpattison <mr.pattison@gmail.com>
2019-09-29 20:56:20 +0100

    switch to 1D array for Fable

--- src/Reversi/Position.fs
+++ src/Reversi/Position.fs
@@ -1,4 +1,4 @@
-module FableReversi.Reversi.Position
+namespace FableReversi.Reversi
 
 [<Struct>]
 type Colour = Black | White
@@ -9,7 +9,9 @@ type Square =
     | Empty
 
 [<Struct>]
-type Location = { X: int; Y: int }
+type Location =
+    | Location of int * int
+    static member (+) ((Location (x1, y1)), (Location (x2, y2))) = Location (x1 + x2, y1 + y2)
 
 type PossibleMove =
     {
@@ -18,75 +20,86 @@ type PossibleMove =
     }
 
 type Position =
-    private {
+    {
         NextToMove: Colour
         Size: int
-        Squares: Square [,]
+        Squares: Square []
     }
 
-let createEmpty boardSize =
-    {
-        NextToMove = Black
-        Size = boardSize
-        Squares = Array2D.create boardSize boardSize Empty
-    }
+module Position =
+    let createEmpty boardSize =
+        {
+            NextToMove = Black
+            Size = boardSize
+            Squares = Array.create (boardSize * boardSize) Empty
+        }
 
-let createStarting() =
-    let position = createEmpty 8
+    let inline indexOf size (Location (x, y)) = x + y * size
 
-    position.Squares.[3, 3] <- Piece Black
-    position.Squares.[4, 4] <- Piece Black
-    position.Squares.[3, 4] <- Piece White
-    position.Squares.[4, 3] <- Piece White
+    let private setPiece position location square =
+        position.Squares.[indexOf position.Size location] <- square
 
-    position
+    let private getPiece position location =
+        position.Squares.[indexOf position.Size location]
 
-let numPieces (squares: Square [,]) size colour =
-    let mutable count = 0
+    let createStarting() =
+        let position = createEmpty 8
 
-    for x in 0..(size - 1) do
-        for y in 0..(size - 1) do
-            if squares.[x, y] = Piece colour then count <- count + 1
-    
-    count
+        Piece Black |> setPiece position (Location (3, 3))
+        Piece Black |> setPiece position (Location (4, 4))
+        Piece White |> setPiece position (Location (3, 4))
+        Piece White |> setPiece position (Location (4, 3))
 
-let pieceAt position location =
-    position.Squares.[location.X, location.Y]
+        position
 
-let opposite colour =
-    match colour with
-    | White -> Black
-    | Black -> White
+    let numPieces position colour =
+        let mutable count = 0
 
-let private wouldFlip position x y (xInc, yInc) =
-    let mutable x' = x + xInc
-    let mutable y' = y + yInc
-    let mutable foundEmpty = false
-    let mutable foundColour = false
+        for i in 0..((position.Size - 1) * (position.Size - 1)) do
+            if position.Squares.[i] = Piece colour then count <- count + 1
+        
+        count
 
-    [ while (x' >= 0 && x' < position.Size && y' >= 0 && y' < position.Size && not foundEmpty && not foundColour) do
-        match position.Squares.[x, y] with
-        | Empty -> foundEmpty <- true
-        | Piece c when c = position.NextToMove -> foundColour <- true
-        | _ -> yield { X = x'; Y = y' }
-        x' <- x' + xInc
-        y' <- y' + yInc
-    ]
+    let pieceAt position location =
+        position.Squares.[indexOf position.Size location]
 
-let private directions = [ (1, 0); (1, 1); (0, 1); (-1, 1); (-1, 0); (-1, -1); (0, -1); (1, -1) ]
+    let opposite colour =
+        match colour with
+        | White -> Black
+        | Black -> White
 
-let private isPossibleMove position x y =
-    if position.Squares.[x, y] = Empty then
-        None
-    else
-        let flips = directions |> List.collect (wouldFlip position x y)
-        if flips.IsEmpty then None else Some flips
+    let private isOnBoard size (Location (x, y)) =
+        x >= 0 && x < size && y >= 0 && y < size
 
-let possibleMoves position =
-    [
-        for x in 0..(position.Size - 1) do
-            for y in 0..(position.Size - 1) do
-                match isPossibleMove position x y with
-                | Some flips -> yield { MoveLocation = { X = x; Y = y }; Flips = flips }
-                | None -> ()
-    ]
+    let private wouldFlip position location direction =
+        let mutable location' = location + direction
+        let mutable foundEmpty = false
+        let mutable foundColour = false
+
+        [ while (isOnBoard position.Size location' && not foundEmpty && not foundColour) do
+            match getPiece position location' with
+            | Empty -> foundEmpty <- true
+            | Piece c when c = position.NextToMove -> foundColour <- true
+            | _ -> yield location'
+            location' <- location' + direction
+        ]
+
+    let private directions =
+        [ (1, 0); (1, 1); (0, 1); (-1, 1); (-1, 0); (-1, -1); (0, -1); (1, -1) ]
+        |> List.map Location
+
+    let private isPossibleMove position location =
+        if getPiece position location = Empty then
+            let flips = directions |> List.collect (wouldFlip position location)
+            if flips.IsEmpty then None else Some flips
+        else
+            None
+
+    let possibleMoves position =
+        [
+            for x in 0..(position.Size - 1) do
+                for y in 0..(position.Size - 1) do
+                    match isPossibleMove position (Location (x, y)) with
+                    | Some flips -> yield { MoveLocation = (Location (x, y)); Flips = flips }
+                    | None -> ()
+        ]
