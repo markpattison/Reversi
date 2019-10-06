0bf964993f41d478c37a120b1ecdf59015d16d7b
markpattison <mr.pattison@gmail.com>
2019-10-06 19:47:01 +0100

    TEMP: diagnose CI issue

--- build.fsx
+++ build.fsx
@@ -70,6 +70,7 @@ Target.create "InstallClient" (fun _ ->
 )
 
 Target.create "Build" (fun _ ->
+    runTool "dir" @"./src/FableReversi/" __SOURCE_DIRECTORY__
     runTool npxTool "webpack-cli -p" __SOURCE_DIRECTORY__
 )
 
