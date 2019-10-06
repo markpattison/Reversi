4c969f9b80677c926745c467f2bb0a7892ce22fa
markpattison <mr.pattison@gmail.com>
2019-10-06 12:17:40 +0100

    show score and nicer next move indicator

--- src/FableReversi/View.fs
+++ src/FableReversi/View.fs
@@ -21,6 +21,13 @@ let possibleMoveCellProps = toCellProps "#00d000"
 let possibleMoveHoverCellProps = toCellProps "#00f000"
 let wouldFlipCellProps = toCellProps "#00d000"
 
+let button txt onClick =
+    Button.button
+        [ Button.IsFullWidth
+          Button.Color IsPrimary
+          Button.OnClick onClick ]
+        [ str txt ]
+
 let showSquare dispatch humanPlaying (location, square, view) =
     let cellProps : IHTMLProp list =
         match view with
@@ -53,23 +60,26 @@ let showBoard dispatch humanPlaying boardView =
     Table.table [ Table.IsBordered; Table.IsNarrow; Table.Props [ Style [ TableLayout "fixed"; Height "400px"; Width "400px" ] ] ]
         [ tbody [] rows ]
 
-let button txt onClick =
-    Button.button
-        [ Button.IsFullWidth
-          Button.Color IsPrimary
-          Button.OnClick onClick ]
-        [ str txt ]
 
 let content (model : Model) (dispatch : Msg -> unit) =
     let humanPlaying = match model.CurrentPlayer with | Human -> true | _ -> false
+    let blackToPlay = model.Board.NextToMove = Black && model.GameState <> Finished
+    let whiteToPlay = model.Board.NextToMove = White && model.GameState <> Finished
 
     Columns.columns []
       [ Column.column [ Column.Width (Screen.All, Column.Is9) ]
-          [ yield showBoard dispatch humanPlaying model.BoardView
-            yield p [] [ sprintf "Next to play: %O" model.Board.NextToMove |> str ]
-            yield p [] [ sprintf "State: %O" (model.Board.GameState()) |> str ] ]
+          [ showBoard dispatch humanPlaying model.BoardView ]
         Column.column []
-            [ if humanPlaying && model.GameState = OngoingSkipMove then yield button "Skip move" (fun _ -> dispatch (GameAction SkipMove))
+            [ yield
+                p []
+                    [ yield sprintf "Black: %i " (model.Board.NumPieces Black) |> str
+                      if blackToPlay then yield Fa.i [ Fa.Solid.ArrowAltCircleLeft ] [] ]
+              yield
+                p []
+                    [ yield sprintf "White: %i " (model.Board.NumPieces White) |> str
+                      if whiteToPlay then yield Fa.i [ Fa.Solid.ArrowAltCircleLeft ] [] ]
+              yield br []
+              if humanPlaying && model.GameState = OngoingSkipMove then yield button "Skip move" (fun _ -> dispatch (GameAction SkipMove))
               if model.GameState = Finished then yield button "Restart game" (fun _ -> dispatch RestartGame) ] ]
 
 let view (model : Model) (dispatch : Msg -> unit) =
