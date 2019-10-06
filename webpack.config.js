853f96fd2d297d0e1abfd0f22473e183286e5ce5
markpattison <mr.pattison@gmail.com>
2019-10-06 18:57:56 +0100

    correct deployment folder

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ var CONFIG = {
     indexHtmlTemplate: './src/FableReversi/index.html',
     fsharpEntry: './src/FableReversi/FableReversi.fsproj',
     cssEntry: './src/FableReversi/style.scss',
-    outputDir: './src/FableReversi/deploy',
+    outputDir: './deploy',
     assetsDir: './src/FableReversi/public',
     devServerPort: 8080,
     // When using webpack-dev-server, you may need to redirect some calls
