dac2460d03c8fa3802e310b337f52e40b0a5a232
--global <mr.pattison@gmail.com>
2020-01-11 18:41:39 +0000

    corrected MCTS algorithm

--- src/Reversi/Computer/MCTS.fs
+++ src/Reversi/Computer/MCTS.fs
@@ -79,8 +79,8 @@ and Node(parent:Node option,random:Random,board:Board) =
                         let opposite = currentBoard.NextToMove.Opposite
                         currentBoard <- { currentBoard with NextToMove = opposite }
                     else
-                        let numBlack = Bitwise.countStones board.BlackSquares
-                        let numWhite = Bitwise.countStones board.WhiteSquares
+                        let numBlack = Bitwise.countStones currentBoard.BlackSquares
+                        let numWhite = Bitwise.countStones currentBoard.WhiteSquares
                         if numBlack > numWhite then
                             this.BackProp -1.
                         elif numWhite > numBlack then
