3007c45b1363ebe3d51846f3833b4bd61ddfcb00
--global <mr.pattison@gmail.com>
2020-01-27 15:06:46 +0000

    uncommenting tests

--- src/TestComputer/Program.fs
+++ src/TestComputer/Program.fs
@@ -5,8 +5,8 @@ open Expecto
 [<EntryPoint>]
 let main args =
     testList "All tests" [
-        //TestBoard.allTests
-        //TestMinimax.allTests
+        TestBoard.allTests
+        TestMinimax.allTests
         TestMCTS.allTests
         //TestTournament.allTests  // slow!
     ]
